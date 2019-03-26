7186dd2b906e1e5b00d7793b8dc0428df0bf0614
Colin Jaffe <colin.jaffe@gmail.com>
2019-03-26 10:07:17 -0400

    adds data.js

---
+++ data.js
@@ -0,0 +1,69 @@
+const headInstructors = [
+    {
+        firstName: 'Colin',
+        lastName: 'Jaffe',
+        term: 1,
+        usesLinux: true,
+        topicsCovered: [
+            'Responsive Design',
+            'JavaScript',
+            'Bash',
+            'Git'
+        ]
+    },
+    {
+        firstName: 'Yuri',
+        lastName: 'Shkoda',
+        term: 2,
+        usesLinux: false,
+        topicsCovered: [
+            'Databases',
+            'Routing',
+            'Beginning React',
+            'Algorithms and Data Structures'
+        ]
+    },
+    {
+        firstName: 'Pak',
+        lastName: 'Chu',
+        term: 3,
+        usesLinux: false,
+        topicsCovered: [
+            'Advanced React',
+            'Interviewing',
+            'Putting It All Together'
+        ]
+    }
+];
+
+// {
+//     firstName: 'Quentin',
+//     lastName: 'Coldwater',
+//     age: 17,
+//     issues: [
+//         'self-esteem',
+//         'narcissism',
+//         'escapism',
+//         'depression'
+//     ],
+//     abilities: [
+//         'singing badly',
+//         'hyperfocusing',
+//         'devotion',
+//         'knowledge of Fillory'
+//     ]
+// },
+// {
+//     firstName: 'Julia',
+//     lastName: 'Wicker',
+//     age: 17,
+//     issues: [
+//         'self-blame',
+//         'trauma',
+//         'consumed by revenge'
+//     ],
+//     abilities: [
+//         'self-taught magician',
+//         ''
+//     ]
+// }
---
+++ main.js
@@ -0,0 +1,3 @@
+for (let i = 0; i < headInstructors.length; i++) {
+    console.log(headInstructors[i].lastName);
+}
